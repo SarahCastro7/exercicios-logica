ac6265fab13c6c1fe5d8e8494ede118b22eccb3e
SarahCastro7 <sarah.c.nascimento10@gmail.com>
2025-05-27 17:10:46 -0300

    Add files via upload

---
+++ idades - fernando.js
@@ -0,0 +1,11 @@
+
+//ler a idade do usuario 
+// se a idade for maior que 18, dizer que é maior de idade
+// se nao, dizer que é de menor 
+
+let idade = prompt (" qual é a sua idade?")
+if( idade >=18) {
+ alert (" voce é maior") 
+}else{
+    alert (" voce é menor de idade")
+}
